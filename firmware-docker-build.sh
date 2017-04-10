6a47facf45d9c254e3245e064235c8ed969b40fa
Peter Banik <peter@prioritylane.com>
2017-04-10 17:28:02 +0200

    Added Decred parameters

--- firmware-docker-build.sh
+++ firmware-docker-build.sh
@@ -6,7 +6,7 @@ FIRMWARETAG=${1:-master}
 
 docker build -t $IMAGETAG .
 docker run -t -v $(pwd)/output:/output $IMAGETAG /bin/sh -c "\
-	git clone https://github.com/trezor/trezor-mcu && \
+	git clone https://github.com/peterzen/trezor-mcu && \
 	cd trezor-mcu && \
 	git checkout $FIRMWARETAG && \
 	git submodule update --init && \
